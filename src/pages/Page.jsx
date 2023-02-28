5ffca996661861fe7e3eb420673d33f5ccdfde5e
HAMMER17 <plutalov70@gmail.com>
2023-02-28 23:34:06 +0300

    delete spinner

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,34 +1,35 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { ClockLoader } from 'react-spinners'
+// import { ClockLoader } from 'react-spinners'
 import '../style/page.css'
 
 const Page = () => {
-  const [louder, setLouder] = useState(true)
+  // const [louder, setLouder] = useState(true)
   const navigate = useNavigate()
   const data = useSelector(state => state.data.value)
   console.log(data)
   const backPage = () => {
     navigate(-1)
   }
-  setTimeout(() => {
-    setLouder(false)
-  }, 2000)
+  // setTimeout(() => {
+  //   setLouder(false)
+  // }, 2000)
   return (
     <div className='page'>
-      {louder ? <ClockLoader color="#ff0013" size={70} /> :
-        <>
-          <div className="page_left">
-            <button onClick={backPage}>back</button>
-            <img src={data.url} alt="f" />
-          </div>
-          <div className="page_right">
-            <h1>{data.film}</h1>
-            <p>{data.text}</p>
-            <video style={{ width: 500 }} src={data.video} autoPlay />
-          </div>
-        </>}
+      {/* {louder ? <ClockLoader color="#ff0013" size={70} /> :
+        <> */}
+      <div className="page_left">
+        <button onClick={backPage}>back</button>
+        <img src={data.url} alt="f" />
+      </div>
+      <div className="page_right">
+        <h1>{data.film}</h1>
+        <p>{data.text}</p>
+        <video style={{ width: 500 }} src={data.video} autoPlay />
+        <h4 style={{ margin: 10 }}>{data.title}</h4>
+      </div>
+      {/* </>} */}
     </div>
   )
 }
--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -9,7 +9,7 @@ const Play = () => {
     <div className='play'>
       <h2 style={{ marginTop: 100 }}>{data.film}</h2>
       <video src={data.video} loading='lazy' autoPlay style={{ border: 'none', margin: 20 }} title='title' />
-
+      <h4>{data.title}</h4>
     </div>
   )
 }
