3d09db93e4d00c53536de61d5da05a9d8b5040d6
HAMMER17 <plutalov70@gmail.com>
2023-03-01 19:00:54 +0300

    moves show video

--- src/pages/Moves.jsx
+++ src/pages/Moves.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import king from '../video/king.mp4'
 import alice from '../video/alica.mp4'
 import aladdin from '../video/aladdin.mp4'
@@ -8,19 +8,26 @@ import heard from '../video/heard.mp4'
 
 import '../style/moves.css'
 
-const Moves = () => {
-  let array = [king, alice, heard, chip, fire, aladdin]
 
+const Moves = () => {
+  const [show, setShow] = useState(false)
+  const [url, setUrl] = useState('')
+  let array = [king, alice, heard, chip, fire, aladdin]
+  const showData = (data) => {
+    setShow(!show)
+    setUrl(data)
+  }
   return (
     <div className='move'>
       <h3>Cartoons for Kids</h3>
       {array.map((el, id) => (
-        <div className="move_container" key={id}>
+        <div className="move_container" key={id} onClick={() => showData(el)}>
           <video src={el} muted autoPlay loop />
         </div>
 
       ))}
-
+      {show ? <span className='video' onClick={showData}>
+        <video className='video_url' src={url} autoPlay loop /></span> : null}
     </div>
   )
 }
